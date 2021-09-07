e3e626b48490c2d6ecf7fd46b0b051c86df8cc5b
v0atmage <v0atmage@gmail.com>
2021-09-07 02:33:56 -0400

    Kill iframely

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+CWD=/lemmy
+cd $CWD
+
 VERSION='0.12.0-no-slur-check'
 
 docker kill lemmy_lemmy_1
 docker kill lemmy_lemmy-ui_1
 docker kill lemmy_pictrs_1
+docker kill lemmy_iframely_1
 
 docker-compose -f docker-compose-$VERSION.yml up -d
