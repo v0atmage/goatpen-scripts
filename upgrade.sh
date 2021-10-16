60d1f9c3586c49fa48b81da0ceaa08a7bf45324d
v0atmage <v0atmage@gmail.com>
2021-10-16 17:05:10 -0400

    Document steps

--- upgrade.sh
+++ upgrade.sh
@@ -5,9 +5,17 @@ cd $CWD
 
 VERSION='0.12.0-no-slur-check'
 
+# Kill lemmy
 docker kill lemmy_lemmy_1
+
+# Kill the ui
 docker kill lemmy_lemmy-ui_1
+
+# Kill the picture db
 docker kill lemmy_pictrs_1
+
+# Kill iframely (permanently- it's unused in 0.12+)
 docker kill lemmy_iframely_1
 
+# Start the new version which will trigger an upgrade on the db
 docker-compose -f docker-compose-$VERSION.yml up -d
